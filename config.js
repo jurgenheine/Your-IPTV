6619d868021e9217287db880da56bf9eeb2efcdb
Jurgen Heine <17513366+jurgenheine@users.noreply.github.com>
2025-04-18 15:36:57 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-var env = process.env.NODE_ENV ? 'beamup':'local';
+var env = process.env.NODE_ENV ? 'production':'local';
 
 var config = {
     addon: 'started',
@@ -6,14 +6,14 @@ var config = {
 
 switch (env) {
     //Public server build.
-    case 'beamup':
-		config.port = process.env.PORT
-        config.local = "youriptv.fam-heine.nl"
+    case 'production':
+	config.port = process.env.PORT
+        config.local = process.env.NODE_ENV
         break;
 
     //Local sever build.
     case 'local':
-		config.port = 3649
+	config.port = 3649
         config.local = "http://127.0.0.1:" + config.port;
         break;
 }
